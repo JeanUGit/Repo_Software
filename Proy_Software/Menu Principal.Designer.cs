b65034245f269b677b7fd554aceeeb96756f166d
Jean Carlos <jeancarlosquejadatoro@gmail.com>
2019-12-14 15:31:32 -0500

    Subo un archivo de comunicación

--- Proy_Software/Menu Principal.Designer.cs
+++ Proy_Software/Menu Principal.Designer.cs
@@ -44,7 +44,6 @@
             this.reservasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.devolucionesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.cuentasXPagarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -163,22 +162,11 @@
             this.cuentasXPagarToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
             this.cuentasXPagarToolStripMenuItem.Text = "Cuentas x pagar";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(292, 202);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Sergio";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
             // Menu_Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip2);
             this.Name = "Menu_Principal";
             this.Text = "Menu_Principal";
@@ -206,6 +194,5 @@
         private System.Windows.Forms.ToolStripMenuItem reservasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem devolucionesToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem cuentasXPagarToolStripMenuItem;
-        private System.Windows.Forms.Label label1;
     }
 }
