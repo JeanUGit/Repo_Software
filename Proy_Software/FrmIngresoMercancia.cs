303a3bc6d46313915cbd70868b53084493384ca1
unknown <tascon20sergio11@gmail.com>
2019-12-14 15:05:42 -0500

    subo por que subo

---
+++ Proy_Software/FrmIngresoMercancia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proy_Software
+{
+    public partial class FrmIngresoMercancia : Form
+    {
+        public FrmIngresoMercancia()
+        {
+            InitializeComponent();
+        }
+    }
+}
